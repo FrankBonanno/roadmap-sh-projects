c31050b1955c444ed10b32fca58e3bc2d0a10e0a
FrankBonanno <fb0023@mix.wvu.edu>
2024-09-19 14:52:44 -0400

    Load and Save functions

---
+++ task-tracker-cli/tracker.py
@@ -0,0 +1,13 @@
+import json
+import os
+
+def load_tasks():
+  if not os.path.exists("tasks.json"):
+    with open('tasks.json', 'w') as f:
+      json.dump([], f)
+  with open("tasks.json", "r") as f:
+    return json.load(f)
+  
+def save_tasks(tasks):
+  with open("tasks.json", "w") as f:
+    json.dump(tasks, f, indent=2)
